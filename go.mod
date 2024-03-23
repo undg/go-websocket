e33afa9aa1547641d987134afab7331d8053e6b7
undg <und3rdg@gmail.com>
2024-03-23 21:40:33 +0000

    Upgrade rest endpoint to websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/undg/go-websocket
 
 go 1.22.1
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,7 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+
+		// Read messages
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Print for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home Page hit by clien")
@@ -15,7 +43,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Ws Page hit by client")
 
-	fmt.Fprintf(w, "Welcome on 'ws' endpoint")
+	// fmt.Fprintf(w, "Welcome on 'ws' endpoint")
+
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade to WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Client connected")
+	reader(ws)
 }
 
 func setupRoutes() {
